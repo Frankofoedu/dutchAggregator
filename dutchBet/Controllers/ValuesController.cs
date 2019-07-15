7b6bad49bfc1c46a5b125af678476e69f5097461
Okpara Obinna <obainoforgod@gmail.com>
2019-07-15 13:11:21 +0100

    Obinna commit

--- dutchBet/Controllers/ValuesController.cs
+++ dutchBet/Controllers/ValuesController.cs
@@ -10,9 +10,58 @@ namespace dutchBet.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public async System.Threading.Tasks.Task<IEnumerable<string>> GetAsync()
         {
+            var MerryBetPL = await loadMerryBetPremierLeagueAsync();
+
+            if (MerryBetPL.Count>0)
+            {
+                var NairaBetPL = new List<Models.Soccer.Market>();
+
+                foreach (var item in MerryBetPL)
+                {
+                    NairaBetPL.Add(new Models.Soccer.Market() {
+                        date = item.date,
+                        odds = item.odds,
+                        time = item.time,
+                        _event = item._event
+                    });
+                }
+
+
+
+
+            }
+
+
+
             return new string[] { "value1", "value2" };
+        }
+
+        private async System.Threading.Tasks.Task<List<Models.Soccer.Market>> loadMerryBetPremierLeagueAsync()
+        {
+            using (WebClient Wc = new WebClient())
+            {
+                try
+                {
+                    var json = await Wc.DownloadStringTaskAsync("https://odds-scraper.herokuapp.com/api/merrybet/soccer/pl");
+                    var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Soccer.Rootobject>(json);
+                    if (obj != null)
+                    {
+                        if (obj.success)
+                        {
+                            return obj.market.ToList();
+                        }
+                    }
+
+                    return null;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
         }
 
         // GET api/values/5
@@ -24,6 +73,7 @@ namespace dutchBet.Controllers
         // POST api/values
         public void Post([FromBody]string value)
         {
+
         }
 
         // PUT api/values/5
