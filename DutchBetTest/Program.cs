80f69818935a5838cb90cc361ba0f1aea23dfa2f
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-07-19 18:59:37 +0100

    bug fixes

--- DutchBetTest/Program.cs
+++ DutchBetTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -12,19 +14,38 @@ namespace DutchBetTest
 {
     class BetPawaOverview
     {
+        public int Id { get; set; }
         public string League { get; set; }
         public List<BetPawaMatches> Matches { get; set; }
     }
 
     class BetPawaMatches
     {
+        public int Id { get; set; }
         public string TeamNames { get; set; }
+        public string  TimeOfMatch { get; set; }
+        public string DateOfMatch { get; set; }
         public List<BetPawaOdds> Odds { get; set; }
     }
     class BetPawaOdds
     {
+        public int Id { get; set; }
+        public string MainType { get; set; }
+        public string Type { get; set; }
         public string Selection { get; set; }
         public string Value { get; set; }
+    }
+
+    class BetPawaContext : DbContext
+    {
+        public BetPawaContext(): base(@"Server =.\SQLEXPRESS; Database=BetPawa;Trusted_Connection=True;")
+        {
+
+        }
+        public DbSet<BetPawaOverview> BetPawaOverviews { get; set; }
+        public DbSet<BetPawaMatches> BetPawaMatches { get; set; }
+        public DbSet<BetPawaOdds> BetPawaOdds { get; set; }
+
     }
     class Program
     {
@@ -47,7 +68,7 @@ namespace DutchBetTest
             ChromeOptions opt = new ChromeOptions();
 
             opt.AddArgument("--headless");
-
+            
            
 
             var betOverview = new List<BetPawaOverview>();
@@ -60,6 +81,7 @@ namespace DutchBetTest
 
                 //gets all sidebar leagues
                 var x = driver.FindElements(By.XPath("//*[@id=\"Popular-League-List\"]/li/a"));
+
 
 
                 var links = new List<string>();
@@ -78,41 +100,142 @@ namespace DutchBetTest
                     //list of match showing win, draw and lose
                     var matches = driver.FindElements(By.CssSelector("div[class='events-container prematch']"));
                     // var t = matches[0].FindElement(By.TagName("h3"));
-                    betOverview[i].Matches = GetMatches(matches);
-
+                    betOverview[i].Matches = GetMatches(matches, driver);
+                   
                 }
 
+               
 
                 //*[@id="Popular-League-List"]/li[1]/a
-                Console.Write(x);
+                Console.Write("Finished Running Scraper");
                 Console.Read();
             }
 
-            List<BetPawaMatches> GetMatches(ReadOnlyCollection<IWebElement> x)
+            List<BetPawaMatches> GetMatches(ReadOnlyCollection<IWebElement> x, ChromeDriver driver)
             {
+                var Mlinks = new List<string>();
                 var bpMatches = new List<BetPawaMatches>();
+
+                string teamNames ;
+                string matchTime;
+                string matchDate;
                 for (int i = 0; i < x.Count; i++)
                 {
-                    var teamNames = x[i].FindElement(By.TagName("h3"));
 
-                    var selections = x[i].FindElements(By.ClassName("event-selection"));
-                    var odds = x[i].FindElements(By.ClassName("event-odds"));
+                    //get matches links
+                    Mlinks.Add(x[i].FindElement(By.ClassName("event-match")).GetAttribute("href"));
 
-                    var selectionAndOdds = GetOdds(selections, odds);
-                    bpMatches.Add(new BetPawaMatches { TeamNames = teamNames.Text, Odds = selectionAndOdds });
-                    //  GetOdds(x[0]);
                 }
+
+
+                //go to individual match page
+
+                for (int i = 0; i < Mlinks.Count; i++)
+                {
+
+
+
+                    driver.Navigate().GoToUrl(Mlinks[i]);
+
+                    // driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 10);
+
+                    Thread.Sleep(5000);
+
+                   teamNames = driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div[1]/div[4]/div/div[1]/div/h2")).Text;
+
+                    matchTime = driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div[1]/div[4]/div/div[1]/div/span/span[2]")).Text;
+
+                    matchDate = driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div[1]/div[4]/div/div[1]/div/span/span[1]")).Text;
+
+                   var SelectAndOddsHtml = driver.FindElements(By.CssSelector("div[class='events-container']"));
+
+                    var SelectAndOdds = GetOdds(SelectAndOddsHtml);
+
+                    var newMatch = new BetPawaMatches { DateOfMatch = matchDate, TeamNames = teamNames, TimeOfMatch = matchTime, Odds = SelectAndOdds };
+                    bpMatches.Add(newMatch);
+
+                    using (var db = new BetPawaContext())
+                    {
+                        db.BetPawaMatches.Add(newMatch);
+
+                        db.SaveChanges();
+                    }
+                }
+
 
                 return bpMatches; 
             }
             
-           List<BetPawaOdds> GetOdds(ReadOnlyCollection<IWebElement> selects, ReadOnlyCollection<IWebElement> odds)
+           List<BetPawaOdds> GetOdds(ReadOnlyCollection<IWebElement> selectsnodds)
             {
                 var lstBets = new List<BetPawaOdds>();
-                for (int i = 0; i < selects.Count; i++)
+
+                for (int i = 0; i < selectsnodds.Count; i++)
                 {
-                    lstBets.Add(new BetPawaOdds { Selection = selects[i].Text, Value = odds[i].Text });
+                    var container = selectsnodds[i];
+
+                    var mainType = container.FindElement(By.TagName("h3")).Text;
+                    var subcontainers = container.FindElements(By.ClassName("events-sub-container"));
+
+                    string selectionType = "";
+                    for (int j = 0; j < subcontainers.Count; j++)
+                    {
+                        var bsubCont = subcontainers[j];
+                        string sType;
+
+                        try
+                        {
+
+                             sType = bsubCont.FindElement(By.TagName("h4")).Text;
+                        }
+                        catch (NoSuchElementException)
+                        {
+                            sType = null;
+                        }
+
+
+                        if (string.IsNullOrEmpty(sType))
+                        {
+                            sType = selectionType;
+                        }
+                        else
+                        {
+                            selectionType = sType;
+                        }
+
+                        var listBetHtml = bsubCont.FindElements(By.CssSelector(".event-bet"));
+
+                        for (int k  = 0; k   < listBetHtml.Count; k++)
+                        {
+                            var html = listBetHtml[k];
+                            string slct = null;
+                            string odd = null;
+
+                            try
+                            {
+
+                                 slct = html.FindElement(By.ClassName("event-selection")).Text;
+                                 odd = html.FindElement(By.ClassName("event-odds")).Text;
+                            }
+                            catch (Exception)
+                            {
+
+                                continue;
+                            }
+
+                            if (!(string.IsNullOrEmpty(slct) || string.IsNullOrEmpty(odd)))
+                            {
+                                lstBets.Add(new BetPawaOdds { MainType = mainType, Type = sType, Selection = slct, Value = odd });
+                            }
+
+
+                        }
+                    }
                 }
+                //for (int i = 0; i < selects.Count; i++)
+                //{
+                //    lstBets.Add(new BetPawaOdds { Selection = selects[i].Text, Value = odds[i].Text });
+                //}
 
                 return lstBets;
             }
