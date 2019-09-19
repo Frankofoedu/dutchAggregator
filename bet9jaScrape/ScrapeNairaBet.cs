d4fced93a74ce57beba61f5e72ef859d0c2a88eb
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-19 09:36:26 +0100

    addded nairabet initial fille

---
+++ bet9jaScrape/ScrapeNairaBet.cs
@@ -0,0 +1,20 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    public class ScrapeNairaBet
+    {
+        public static Task< List<BetMatch>> Scrape(HttpClient client)
+        {
+            var url = "https://www.nairabet.com/UK/Sports#action=filtered-market-group-list&datefilter=20190918";
+
+
+        }
+    }
+}
