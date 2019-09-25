2fc2bb86f8b0036717b1c55ae9ec80fcc491d985
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-25 16:15:42 +0100

    updated 1xbet and fixed bugs

--- bet9jaScrape/Scrape1XBet.cs
+++ bet9jaScrape/Scrape1XBet.cs
@@ -19,152 +19,126 @@ namespace Scraper
         {
             //url to get all football markets
             var url = "https://1xbet.ng/LineFeed/GetChampsZip?sport=1&lng=en&tf=2200000&tz=1&country=132&partner=159&virtualSports=true";
+
             var betMatches = new List<BetMatch>();
             try
             {
                 Console.WriteLine("Started scraping 1xBet");
+
                 //get todays data
                 var response = await client.GetStringAsync(url);
 
 
+
+                //get structure of odds
+                var ddt = await GetBetModelJsonAsync(client);
+                
+                var betsName =  GetBetsName(ddt);
+                var betsNameModelGroup = GetBetsNameModel(ddt);
+
                 //var response = responseTask.Result;
                 var todayData = JsonConvert.DeserializeObject<FootBallTypes>(response);
 
-                if (todayData != null)
+                if (todayData == null) return null;
+
+
+
+                //loop through leagues/countries
+                foreach (var market in todayData.Value)
                 {
-                    //loop through leagues/countries
-                    foreach (var market in todayData.Value)
+
+                    //removes unneccesary leagues
+                    if (market.L.ToLower().Trim().Contains("statistics round") || market.L.ToLower().Trim().Contains("special"))
+                        continue;
+                    
+                   
+                    
+                    //get country and league
+                    var arr = market.L.Split('.');
+
+
+                    string country = "";
+                    string league = "";
+                    league = GetLeagueAndOrCountryNames(market, arr, ref country);
+
+                    //get matches in league
+                  var mUrl = $"https://1xbet.ng/LineFeed/Get1x2_VZip?champs={market.Id}&count=10&lng=en&mode=4&top=true&partner=159";
+
+                    var marketResponse = await client.GetAsync(mUrl);
+
+                    
+                    if (!marketResponse.IsSuccessStatusCode)
+                        continue;
+                    
+
+                    var dt = await marketResponse.Content.ReadAsStringAsync();
+
+                    var matchData = JsonConvert.DeserializeObject<SecondData>(dt);
+
+                    //create list of match tasks
+                    var allMatchTasks = matchData.Value.Select(x => client.GetAsync($"https://1xbet.ng/LineFeed/GetGameZip?id={x.Ci}&lng=en&cfview=0&isSubGames=true&GroupEvents=true&allEventsGroupSubGames=true&countevents=250&partner=159"));
+
+                    //get list of matche and thier data
+                    var allMatches = await Task.WhenAll(allMatchTasks);
+
+
+                    foreach (var item in allMatches)
                     {
-                        //removes unneccesary leagues
-                        if (!market.L.ToLower().Trim().Contains("statistics round") && !market.L.ToLower().Trim().Contains("special"))
+                        if (!item.IsSuccessStatusCode)
                         {
-                            //get country and league
-                            var arr = market.L.Split('.');
-                            string country = "";
-                            string league = "";
-                            if (arr.Length > 1)
-                            {
-                                country = arr[0].Trim();
-                                league = arr[1].Trim();
-                            }
-                            else
-                            {
-                                //sets the league if no seperator exists
-                                /*
-                                 e.g "L": "Copa Libertadores",
-                                     "L": "South America Cup",
-                                 */
-                                league = market.L;
-                            }
-
-                            var s = market.L.Trim().Replace(" ", "-");
-                            //get matches in league
-
-                            var mUrl = $"https://1xbet.ng/LineFeed/Get1x2_VZip?champs={market.Id}&count=10&lng=en&mode=4&top=true&partner=159";
-
-                            var marketResponse = await client.GetAsync(mUrl);
-
-                            if (marketResponse.IsSuccessStatusCode)
-                            {
-                                var dt = await marketResponse.Content.ReadAsStringAsync();
-
-                                var data = JsonConvert.DeserializeObject<SecondData>(dt);
-
-                                //create list of match tasks
-                                var allMatchTasks = data.Value.Select(x => client.GetAsync($"https://1xbet.ng/LineFeed/GetGameZip?id={x.Ci}&lng=en&cfview=0&isSubGames=true&GroupEvents=true&allEventsGroupSubGames=true&countevents=250&partner=159"));
-
-                                var allMatches = await Task.WhenAll(allMatchTasks);
-                                
-
-                                foreach (var item in allMatches)
-                                {
-                                    if (item.IsSuccessStatusCode)
-                                    {
-                                        var singleMatchdata = await item.Content.ReadAsStringAsync();
-
-                                        var singleMatch = JsonConvert.DeserializeObject<_1xBetMatchData>(singleMatchdata);
-
-                                        //compare dates to get matches for today only.
-
-                                        var date1 = DateTimeOffset.FromUnixTimeSeconds (singleMatch.Value.S).DateTime.AddHours(1);
-                                        var date2 = DateTime.Now;
-
-                                        //to get only todays data
-                                        if (DateTime.Compare(date1, date2) > 0)
-                                        {
-                                            var betMatch = new BetMatch
-                                            {
-                                                Country = country,
-                                                League = league,
-                                                DateTimeOfMatch = date1,
-                                                Site = "1xBet",
-                                                TeamNames = $"{singleMatch.Value.O1} - {singleMatch.Value.O2}"
-                                            };
-
-                                            var betsNamesUrl = "https://xafr-a.akamaihd.net/genfiles/cms/betstemplates/betsNames_full_en.js";
-
-                                            var betsNameResponse = await client.GetAsync(betsNamesUrl);
-
-                                            var betsJson = await betsNameResponse.Content.ReadAsStringAsync();
-
-
-
-
-                                            //get bets name
-                                            var betsNameJson = GetBetsName(betsJson);
-
-                                            //get bets name Model 
-                                           var betsNameModelJson = GetBetsNameModel(betsJson);
-
-                                            var betsName = JsonConvert.DeserializeObject<Dictionary<string, BetsName>>(betsNameJson, Converter.Settings);
-
-                                            var betsNameModel =  JsonConvert.DeserializeObject<Dictionary<string, BetsNameModel>>(betsNameModelJson, BetsNameModel.Converter.Settings);
-
-                                            var betList = new List< BetOdds>();
-                                            foreach (var ge in singleMatch.Value.Ge)
-                                            {
-                                                
-
-                                                foreach (var ee in ge.E)
-                                                {
-                                                    foreach (var eee in ee)
-                                                    {
-                                                        var g = eee.T;
-                                                        var betType = betsNameModel[g.ToString()].N.Replace("^", string.Empty);
-
-                                                        var odd = eee.C;
-                                                        if (eee.P != null)
-                                                        {
-                                                            betType.Replace("()", eee.P.ToString());
-                                                        }
-
-
-                                                        betList.Add(new BetOdds { MainType = betType, Selection = betType, Value = odd.ToString() });
-                                                    }
-                                                }
-
-                                                
-
-
-                                            }
-
-                                            betMatch.Odds = betList;
-
-                                            betMatches.Add(betMatch);
-                                        }
-
-
-
-
-                                    }
-                                }
-
-                                return betMatches;
-                            }                                                                                
-
+                            continue;
                         }
+                        var singleMatchdata = await item.Content.ReadAsStringAsync();
+
+                        var singleMatch = JsonConvert.DeserializeObject<_1xBetMatchData>(singleMatchdata);
+
+                        //compare dates to get matches for today only.
+
+                        var date1 = DateTimeOffset.FromUnixTimeSeconds(singleMatch.Value.S).DateTime.AddHours(1);
+                        var date2 = DateTime.Now;
+
+                        //to get matches for only today
+                        if (DateTime.Compare(date1, date2) <= 0)
+                        {
+                            continue;
+                        }
+
+                        var betMatch = new BetMatch
+                        {
+                            Country = country,
+                            League = league,
+                            DateTimeOfMatch = date1,
+                            Site = "1xBet",
+                            TeamNames = $"{singleMatch.Value.O1} - {singleMatch.Value.O2}"
+                        };
+
+
+                        var betList = new List<BetOdds>();
+
+                        foreach (var e in singleMatch.Value.Ge)
+                        {
+                            var bG = e.G;
+
+                            var betGroup = betsNameModelGroup[bG.ToString()].N;
+
+                            foreach (var eee in e.E.SelectMany(ee => ee.Select(eee => eee)))
+                            {
+                                var betType = eee.T.ToString();
+                                var value = eee.C.ToString();
+                                var selection = betsName[betType].N;
+                                betList.Add(new BetOdds { MainType = betGroup, Selection = selection, Type = betGroup, Value = value });
+                            }
+                        }
+
+                        betMatch.Odds = betList;
+
+                        betMatches.Add(betMatch);
                     }
+
+                    return betMatches;
                 }
+
+                return null;
 
             }
             catch (Exception e)
@@ -172,52 +146,78 @@ namespace Scraper
                 Console.WriteLine(e.Message);
                 throw;
             }
-
-            return null;
         }
 
-        private static string GetBetsNameModel(string betsJson)
+        /// <summary>
+        /// gets the file that holds id and names of the odds
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static async Task<string> GetBetModelJsonAsync(HttpClient client)
         {
-            var firstIndex = betsJson.IndexOf("var betsModel =");
+            var url = "https://xafr-a.akamaihd.net/genfiles/cms/betstemplates/betsNames_full_en.js";
 
-            var g = ";\nvar betsModelGroup";
-            var secondIndex = betsJson.IndexOf(g);
+            var response = await client.GetAsync(url);
+
+            return await response.Content.ReadAsStringAsync();
+
+            
+
+        }
+        private static Dictionary<string, BetsNameModel> GetBetsNameModel(string betsJson)
+        {
+            /*
+             betjson => 
+             "var betsModel = {...};
+              var betsModelGroup = {...};"
+        
+             */
+            
+            var firstIndex = betsJson.IndexOf("var betsModel =");            
+            var secondIndex = betsJson.IndexOf(";\nvar betsModelGroup");
 
             var t = betsJson.Remove(0, secondIndex);
-
-
-
             var f = t.Remove(0, 23);
 
             //removes trailing ";"
             f = f.Remove(f.Length - 1);
 
-            return f;
-        }
+            var betsNameModel = JsonConvert.DeserializeObject<Dictionary<string, BetsNameModel>>(f, BetsNameModel.Converter.Settings);
 
-        private static string GetBetsName(string betsNameJson)
+            return betsNameModel;
+        }
+        private static Dictionary<string, BetsName> GetBetsName(string betsNameJson)
         {
 
             var firstIndex = betsNameJson.IndexOf($";\nvar betsModelGroup");
-            var t = betsNameJson.Remove(firstIndex);
+            var secondIndex = betsNameJson.IndexOf("var betsModel =");
 
+            var t = betsNameJson.Remove(firstIndex);            
+            var f = t.Remove(secondIndex, 15);            
 
-            var g = "var betsModel =";
-            var secondIndex = betsNameJson.IndexOf(g);
+            return  JsonConvert.DeserializeObject<Dictionary<string, BetsName>>(f, Converter.Settings);
 
-            var f  = t.Remove(secondIndex, 15);
-
-            return f;
         }
-
-        private static List<HtmlNode> GetScriptTag(IEnumerable<HtmlNode> ft)
+        private static string GetLeagueAndOrCountryNames(Classes._1XBet.Data market, string[] arr, ref string country)
         {
+            string league;
+            if (arr.Length > 1)
+            {
+                country = arr[0].Trim();
+                league = arr[1].Trim();
+            }
+            else
+            {
+                //sets the league if no seperator exists
+                /*
+                 e.g "L": "Copa Libertadores",
+                     "L": "South America Cup",
+                 */
+                league = market.L;
+            }
 
-            return (from item in ft
-                    from atttr in item.Attributes
-                    where atttr.Value == "application/ld+json" && atttr.Name == "type"
-                    select item).ToList();
-
+            return league;
         }
+               
     }
 }
